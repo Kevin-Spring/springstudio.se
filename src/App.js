8de0029cd26993757f688d56bf9c998c86f760cb
Kevin Spring <kevinspring93@hotmail.com>
2021-04-05 16:58:26 +0200

    cookie notice responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Header } from './components/Header'
 import './styles/main.scss'
+import './styles/_cookies.scss'
 import CookieConsent from 'react-cookie-consent'
 
 /* Component header contains all routes and components which routing points at */
@@ -8,24 +9,7 @@ function App() {
     <>
       <Header />
 
-      <CookieConsent
-        exit={{ opacity: 0 }}
-        style={{
-          background: 'rgba(0,0,0,.8)',
-          zIndex: 3,
-          position: 'fixed',
-          bottom: 0,
-          maxWidth: '500px',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          borderTopLeftRadius: '5px',
-          borderTopRightRadius: '5px',
-          fontFamily: 'Roboto, sans-serif',
-          fontSize: '12px',
-          flexFlow: 'row wrap',
-        }}
-        buttonStyle={{ color: '#fffdf7', fontSize: '12px', background: 'transparent', border: '1px solid #fffdf7' }}
-      >
+      <CookieConsent>
         <span> This website uses cookies</span>
       </CookieConsent>
     </>
