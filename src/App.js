041fbf2db2a349a5048ac2330e9f8d1a0dc28dca
Kevin Spring <kevinspring93@hotmail.com>
2021-04-11 11:16:38 +0200

    cookie policy & privacy policy draft

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Header } from './components/Header'
 import './styles/main.scss'
 import './styles/_cookies.scss'
 import CookieConsent from 'react-cookie-consent'
+import { Link } from 'react-router-dom'
 
 /* Component header contains all routes and components which routing points at */
 function App() {
@@ -10,7 +11,12 @@ function App() {
       <Header />
 
       <CookieConsent>
-        <span> This website uses cookies</span>
+        <span>
+          This website uses cookies to improve the user experience.{' '}
+          <Link to='cookies' style={{ textDecoration: 'underline' }}>
+            Read more
+          </Link>
+        </span>
       </CookieConsent>
     </>
   )
