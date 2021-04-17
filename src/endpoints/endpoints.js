17ab40f13715e2719a4353837df9af56c4375a12
Kevin Spring <kevinspring93@hotmail.com>
2021-04-17 09:51:09 +0200

    development & production endpoints

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -1,4 +1,9 @@
 //Endpoints for the get-actions, fetching info from wp headless api
+
+/* 
+
+PRODUCTION
+
 export const endpoints = [
   {
     type: 'post type - main pages',
@@ -39,5 +44,50 @@ export const endpoints = [
   {
     type: 'single post - studio 3',
     url: 'https://dev.springstudio.se/wp-json/wp/v2/studio_single/332',
+  },
+] */
+
+/* DEVELOPMENT */
+
+export const endpoints = [
+  {
+    type: 'post type - main pages',
+    url: 'http://localhost/developement/springstudio.se/index.php/wp-json/wp/v2/main',
+  },
+  {
+    type: 'post type - studios',
+    url: 'http://localhost/developement/springstudio.se/index.php/wp-json/wp/v2/studios',
+  },
+  {
+    type: 'post type - contact',
+    url: 'http://localhost/developement/springstudio.se/index.php/wp-json/wp/v2/contact',
+  },
+  {
+    type: 'navigation menu',
+    url: 'http://localhost/developement/springstudio.se/wp-json/wp/v2/menus/primary-menu',
+  },
+  {
+    type: 'contact & booking form',
+    url: 'http://localhost/developement/springstudio.se/wp-json/contact-form-7/v1/contact-forms/96/feedback',
+  },
+  {
+    type: 'logo inverted',
+    url: 'http://localhost/developement/springstudio.se/wp-content/uploads/2021/02/logo-inverted-1.png',
+  },
+  {
+    type: 'post type',
+    url: 'http://localhost/developement/springstudio.se/wp-json/wp/v2/studio_single/',
+  },
+  {
+    type: 'single post - studio 1',
+    url: 'http://localhost/developement/springstudio.se/wp-json/wp/v2/studio_single/156',
+  },
+  {
+    type: 'single post - studio 2',
+    url: 'http://localhost/developement/springstudio.se/wp-json/wp/v2/studio_single/331',
+  },
+  {
+    type: 'single post - studio 3',
+    url: 'http://localhost/developement/springstudio.se/wp-json/wp/v2/studio_single/332',
   },
 ]
