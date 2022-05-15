af12d102af374cc45842b15768708ba6434870be
Kevin Spring <kevinspring93@hotmail.com>
2022-05-15 13:14:24 +0200

    ready for prod

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,91 @@
-import React, { useRef, useEffect, useState } from 'react'
-import { NavLink, useLocation, useHistory, Link } from 'react-router-dom'
-import { IoTriangleOutline } from 'react-icons/io5'
+import React, { useRef, useEffect, useState } from "react";
+import { NavLink, useLocation, useHistory, Link } from "react-router-dom";
+import { IoTriangleOutline } from "react-icons/io5";
 //import { Logo } from './Logo'
-import { useFetchNav } from './useFetchNav'
-import { useNavbar } from './useNavbar'
-import { Power3, TweenLite } from 'gsap'
-import '../styles/css/_navbar.css'
-import { endpoints } from '../endpoints/endpoints'
+import { useFetchNav } from "./useFetchNav";
+import { useNavbar } from "./useNavbar";
+import { Power3, TweenLite } from "gsap";
+import "../styles/css/_navbar.css";
+import { endpoints } from "../endpoints/endpoints";
+import { Socials } from "./Socials";
 
 //Pointing get request at correct endpoint
-const urlMenu = endpoints[3].url
+const urlMenu = endpoints[3].url;
 
 //Using custom hooks useFetchNav & UseNavbar to get menu items and set correct title to each nav-arrow depending on page location
 export const Navbar = () => {
-  const history = useHistory()
-  const location = useLocation()
-  const navbar = useRef(null)
-  const fadeIn = useRef([])
-  fadeIn.current = []
-  const [overlay, setOverlay] = useState(false)
-  const { loadingNav, fetchedNavbarItems } = useFetchNav(urlMenu)
-  const { navbarItems, navbarPaths } = useNavbar(fetchedNavbarItems, loadingNav, location)
+  const history = useHistory();
+  const location = useLocation();
+  const navbar = useRef(null);
+  const fadeIn = useRef([]);
+  fadeIn.current = [];
+  const [overlay, setOverlay] = useState(false);
+  const { loadingNav, fetchedNavbarItems } = useFetchNav(urlMenu);
+  const { navbarItems, navbarPaths } = useNavbar(
+    fetchedNavbarItems,
+    loadingNav,
+    location
+  );
 
-  const addToFadeInNav = el => {
+  const addToFadeInNav = (el) => {
     if (el && !fadeIn.current.includes(el)) {
-      fadeIn.current.push(el)
+      fadeIn.current.push(el);
     }
-  }
+  };
 
   //Function makes navigation with arrowkeys possible & block usage if an input or textarea would be in focus
-  const handleKeyDown = event => {
-    if (event.key === 'ArrowRight') {
-      if (document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA') {
-        return
+  const handleKeyDown = (event) => {
+    if (event.key === "ArrowRight") {
+      if (
+        document.activeElement.tagName === "INPUT" ||
+        document.activeElement.tagName === "TEXTAREA"
+      ) {
+        return;
       }
       if (navbarPaths.rightArrow) {
         history.push({
           pathname: navbarPaths.rightArrow,
-        })
+        });
       }
     }
-    if (event.key === 'ArrowLeft') {
-      if (document.activeElement.tagName === 'INPUT' || document.activeElement.tagName === 'TEXTAREA') {
-        return
+    if (event.key === "ArrowLeft") {
+      if (
+        document.activeElement.tagName === "INPUT" ||
+        document.activeElement.tagName === "TEXTAREA"
+      ) {
+        return;
       }
       if (navbarPaths.leftArrow) {
         history.push({
           pathname: navbarPaths.leftArrow,
-        })
+        });
       }
     }
-  }
+  };
 
   const navbarOpen = () => {
-    setOverlay(!overlay)
-    !overlay ? document.body.classList.add('fixed') : document.body.classList.remove('fixed')
-  }
+    setOverlay(!overlay);
+    !overlay
+      ? document.body.classList.add("fixed")
+      : document.body.classList.remove("fixed");
+  };
 
   //Tiny animations for the navbar & eventlistener for arrowkey-events
   useEffect(() => {
-    fadeIn.current.forEach(item => {
+    fadeIn.current.forEach((item) => {
       TweenLite.to(item, 0.1, {
         opacity: 1,
         delay: 5,
         ease: Power3.easeOut,
-      })
-    })
-    window.addEventListener('keydown', handleKeyDown)
+      });
+    });
+    window.addEventListener("keydown", handleKeyDown);
 
     // cleanup to remove eventlistener, perventing eventlistener to be called upon several pages and instances
     return () => {
-      window.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [navbarPaths])
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [navbarPaths]);
 
   useEffect(() => {
     // if (location.pathname.includes('/studio/')) {
@@ -80,7 +93,7 @@ export const Navbar = () => {
       opacity: 1,
       delay: 4.8,
       ease: Power3.easeOut,
-    })
+    });
     //}
 
     /*
@@ -93,8 +106,10 @@ export const Navbar = () => {
     } */
 
     //remove body fixed when switching page
-    overlay ? document.body.classList.add('fixed') : document.body.classList.remove('fixed')
-  }, [location.pathname])
+    overlay
+      ? document.body.classList.add("fixed")
+      : document.body.classList.remove("fixed");
+  }, [location.pathname]);
 
   return (
     <>
@@ -104,18 +119,28 @@ export const Navbar = () => {
       </NavLink> */}
 
       {/* {!location.pathname.includes('/studio/') ? ( */}
-      <nav className={location.pathname === '/' ? 'primary-nav-container light' : 'primary-nav-container dark'}>
-        <div className={'primary-nav'}>
+      <nav
+        className={
+          location.pathname === "/"
+            ? "primary-nav-container light"
+            : "primary-nav-container dark"
+        }
+      >
+        <div className={"primary-nav"}>
           <ul>
             {/* Conditionally rendering navbar and setting correct menu items depending on page location using custom hooks */}
             {navbarItems.leftItem && (
               <NavLink
                 ref={addToFadeInNav}
-                className={overlay ? 'navbar-left-item navbar-item dark' : 'navbar-left-item navbar-item'}
+                className={
+                  overlay
+                    ? "navbar-left-item navbar-item dark"
+                    : "navbar-left-item navbar-item"
+                }
                 to={{ pathname: navbarPaths.leftArrow }}
               >
                 <li>
-                  <IoTriangleOutline className='angle angle-left' />
+                  <IoTriangleOutline className="angle angle-left" />
                   <span>{navbarItems.leftItem}</span>
                 </li>
               </NavLink>
@@ -123,12 +148,16 @@ export const Navbar = () => {
             {navbarItems.rightItem && (
               <NavLink
                 ref={addToFadeInNav}
-                className={overlay ? 'navbar-right-item navbar-item dark' : 'navbar-right-item navbar-item'}
+                className={
+                  overlay
+                    ? "navbar-right-item navbar-item dark"
+                    : "navbar-right-item navbar-item"
+                }
                 to={{ pathname: navbarPaths.rightArrow }}
               >
                 <li>
                   <span>{navbarItems.rightItem}</span>
-                  <IoTriangleOutline className='angle angle-right' />
+                  <IoTriangleOutline className="angle angle-right" />
                 </li>
               </NavLink>
             )}
@@ -136,34 +165,74 @@ export const Navbar = () => {
         </div>
       </nav>
       {/* ) : ( */}
-      <nav ref={navbar} className={location.pathname === '/' ? 'primary-nav-container-curtain light' : 'primary-nav-container-curtain dark'}>
-        <div className={overlay ? 'hamburger-icon open' : 'hamburger-icon'} onClick={navbarOpen /* () => setOverlay(!overlay) */}>
-          <div className='hamburger'></div>
+      <nav
+        ref={navbar}
+        className={
+          location.pathname === "/"
+            ? "primary-nav-container-curtain light"
+            : "primary-nav-container-curtain dark"
+        }
+      >
+        <div
+          className={overlay ? "hamburger-icon open" : "hamburger-icon"}
+          onClick={navbarOpen /* () => setOverlay(!overlay) */}
+        >
+          <div className="hamburger"></div>
         </div>
-        <div className={overlay ? 'overlay open' : 'overlay'}>
-          <div className='overlay-content'>
-            <Link className={location.pathname === '/' ? 'active' : ''} to='/'>
+        <div className={overlay ? "overlay open" : "overlay"}>
+          <div className="overlay-content">
+            <Link className={location.pathname === "/" ? "active" : ""} to="/">
               Home
             </Link>
-            <Link className={location.pathname === '/booking' ? 'active' : ''} to='/booking'>
+            <Link
+              className={location.pathname === "/booking" ? "active" : ""}
+              to="/booking"
+            >
               Booking
             </Link>
-            <Link className={location.pathname === '/studios' ? 'active' : ''} to='/studios'>
+            <Link
+              className={location.pathname === "/studios" ? "active" : ""}
+              to="/studios"
+            >
               Studios
             </Link>
-            <Link className={location.pathname === '/studio/1' ? 'submenu-item active' : 'submenu-item'} to='/studio/1'>
+            <Link
+              className={
+                location.pathname === "/studio/1"
+                  ? "submenu-item active"
+                  : "submenu-item"
+              }
+              to="/studio/1"
+            >
               Studio 1
             </Link>
-            <Link className={location.pathname === '/studio/2' ? 'submenu-item active' : 'submenu-item'} to='/studio/2'>
+            <Link
+              className={
+                location.pathname === "/studio/2"
+                  ? "submenu-item active"
+                  : "submenu-item"
+              }
+              to="/studio/2"
+            >
               Studio 2
             </Link>
-            <Link className={location.pathname === '/studio/3' ? 'submenu-item active' : 'submenu-item'} to='/studio/3'>
+            <Link
+              className={
+                location.pathname === "/studio/3"
+                  ? "submenu-item active"
+                  : "submenu-item"
+              }
+              to="/studio/3"
+            >
               Studio 3
             </Link>
+            <div className="nav-socials-container">
+              <Socials />
+            </div>
           </div>
         </div>
       </nav>
       {/* )} */}
     </>
-  )
-}
+  );
+};
