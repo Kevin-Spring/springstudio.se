767fc7a4a158241b506de37c467c7616b1979ced
Kevin Spring <56160682+Kevin-Spring@users.noreply.github.com>
2022-12-03 18:55:32 +0100

    added seperate meta titles and descriptions to all pages

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,12 +1,26 @@
 import React, { useEffect } from "react";
 import { Booking } from "../components/Booking";
 import { motion } from "framer-motion";
+import { Helmet } from "react-helmet";
 
 import "../styles/css/_bookingPage.css";
 
 export const BookingPage = ({ transition }) => {
   return (
     <>
+      <Helmet>
+        <title>Spring Studio | Booking</title>
+        <meta
+          name="description"
+          content="Contact us or book your studio here - whether it would be for green screen productions, music rehearsals or fashion shoots. Let us know!"
+        />
+        <meta property="og:title" content="Spring Studio | Booking" />
+        <meta
+          property="og:description"
+          content="Contact us or book your studio here - whether it would be for green screen productions, music rehearsals or fashion shoots. Let us know!"
+        />
+        <link rel="canonical" href="https://springstudio.se/booking" />
+      </Helmet>
       <main className="booking-page">
         <motion.section exit={{ opacity: 0 }} transition={transition}>
           <Booking />
